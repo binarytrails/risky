7cc56ff98c97e8fb8dd2498668b6fc4d3bd37128
Seva Ivanov <seva@tumahn.net>
2018-10-07 22:27:40 -0400

    Implement add/remove players hand & countries methods

--- src/Player.cpp
+++ src/Player.cpp
@@ -3,6 +3,8 @@
 
 #include "Player.h"
 
+using namespace std;
+
 Player::Player()
 {
 }
@@ -16,4 +18,27 @@ Player::~Player()
     for (Card* card: this->cardsHand)
         delete card;
     this->cardsHand.clear();
+}
+
+void Player::addCountry(Country* country)
+{
+    this->countries.push_back(country);
+}
+void Player::removeCountry(Country* country)
+{
+    auto it = find(this->countries.begin(), this->countries.end(), country);
+    if (it != this->countries.end())
+        this->countries.erase(it);
+}
+
+void Player::addCard(Card* card)
+{
+    this->cardsHand.push_back(card);
+}
+
+void Player::removeCard(Card* card)
+{
+    auto it = find(this->cardsHand.begin(), this->cardsHand.end(), card);
+    if (it != this->cardsHand.end())
+        this->cardsHand.erase(it);
 }
--- src/Player.h
+++ src/Player.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <vector>
+#include <algorithm> // find
 #include "Country.h"
 #include "Card.h"
 #include "Dice.h"
@@ -15,6 +16,11 @@ class Player
     public:
         Player();
         ~Player();
+
+        void addCountry(Country* country);
+        void removeCountry(Country* country);
+        void addCard(Card* card);
+        void removeCard(Card* card);
 
         int rollDice();
 
