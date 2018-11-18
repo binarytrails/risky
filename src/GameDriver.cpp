5e9890f12b1e6ac7d7f5188aaf8133790b9a0e20
Seva Ivanov <seva@tumahn.net>
2018-11-17 23:44:44 -0500

    GameDriver: Add 1st arg for debug loglevel

--- src/GameDriver.cpp
+++ src/GameDriver.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 #include "Game.h"
 #include "Player.h"
 
@@ -11,8 +12,18 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     spdlog::set_level(spdlog::level::info);
+
+    if (argc > 1)
+    {
+        string loglevel = argv[1];
+
+        if (loglevel == "debug")
+            spdlog::set_level(spdlog::level::debug);
+    }
+
     Game *game = new Game(Game::UI::SHELL);
     game->play();
+
     delete game;
     return 0;
 }
